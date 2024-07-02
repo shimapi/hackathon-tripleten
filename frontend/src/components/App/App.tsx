3880c4664a409927ecd7e827ee9f787825fe0d1a
Shirley Mallea <85321043+shimapi@users.noreply.github.com>
2024-07-02 01:03:05 -0400

    adding some styles and chart.js example

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,14 +1,17 @@
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
+import Graph from "../Graph/Graph";
+import "./App.scss";
 
 const App = () => {
 	return (
-		<>
+		<div className="container">
 			<Header />
 			<Main />
+			<Graph />
 			<Footer />
-		</>
+		</div>
 	);
 };
 
--- frontend/src/components/Graph/Graph.tsx
+++ frontend/src/components/Graph/Graph.tsx
@@ -1,9 +1,64 @@
 import "./Graph.scss";
+import { Bar } from "react-chartjs-2";
+import {
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	BarElement,
+	Title,
+	Tooltip,
+	Legend,
+} from "chart.js";
+ChartJS.register(
+	CategoryScale,
+	LinearScale,
+	BarElement,
+	Title,
+	Tooltip,
+	Legend
+);
+
+const data = {
+	labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio"],
+	datasets: [
+		{
+			label: "Ventas",
+			data: [12, 19, 3, 5, 2, 3],
+			backgroundColor: [
+				"rgba(255, 99, 132, 0.2)",
+				"rgba(54, 162, 235, 0.2)",
+				"rgba(255, 206, 86, 0.2)",
+				"rgba(75, 192, 192, 0.2)",
+				"rgba(153, 102, 255, 0.2)",
+				"rgba(255, 159, 64, 0.2)",
+			],
+			borderColor: [
+				"rgba(255, 99, 132, 1)",
+				"rgba(54, 162, 235, 1)",
+				"rgba(255, 206, 86, 1)",
+				"rgba(75, 192, 192, 1)",
+				"rgba(153, 102, 255, 1)",
+				"rgba(255, 159, 64, 1)",
+			],
+			borderWidth: 1,
+		},
+	],
+};
+const options = {
+	scales: {
+		y: {
+			beginAtZero: true,
+		},
+	},
+};
 
 const Graph = () => {
-  return (
-    <div>Graph</div>
-  )
-}
+	return (
+		<div>
+			<h1>Chart.js con React JS</h1>
+			<Bar data={data} options={options} />
+		</div>
+	);
+};
 
-export default Graph
+export default Graph;
