9e55aeddf64b327ae6bb6154cb073131bbd89ce4
Shirley Mallea <85321043+shimapi@users.noreply.github.com>
2024-07-03 00:41:36 -0400

    feat: adding form

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -2,12 +2,14 @@ import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
 import Graph from "../Graph/Graph";
+import FileForm from "../FileForm/FileForm";
 import "./App.scss";
 
 const App = () => {
 	return (
 		<div className="container">
 			<Header />
+			<FileForm />
 			<Main />
 			<Graph />
 			<Footer />
---
+++ frontend/src/components/FileForm/FileForm.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./FileForm.scss";
+// import { useHistory, useLocation, useParams, useRouteMatch } from 'react-router-dom';
+
+// Si necesitas definir parámetros para useParams, puedes hacerlo aquí.
+// Por ejemplo, si esperas un parámetro "id" en la ruta, puedes definirlo así:
+// interface RouteParams {
+//   id: string;
+// }
+
+const FileForm: React.FC = () => {
+	// Aquí es donde usarías los hooks, si necesitas acceder a las props del router.
+	// const history = useHistory();
+	// const location = useLocation();
+	// const params = useParams<RouteParams>();
+	// const match = useRouteMatch();
+
+	return (
+		<div className="file">
+			<form
+				action="/upload"
+				method="post"
+				encType="multipart/form-data"
+				className="file__form"
+			>
+				<input type="file" name="file" />
+				<button type="submit">Upload</button>
+			</form>
+		</div>
+	);
+};
+
+export default FileForm;
