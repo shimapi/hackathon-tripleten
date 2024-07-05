2229ef5ae101db07855cd7128bf5253bfe2a70b3
Shirley Mallea <85321043+shimapi@users.noreply.github.com>
2024-07-05 02:39:54 -0400

    feat: django working

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -44,11 +44,12 @@ INSTALLED_APPS = [
 
 ]
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:5175']
+CORS_ALLOWED_ORIGINS = ['http://localhost:5174']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
